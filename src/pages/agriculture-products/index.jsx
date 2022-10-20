1efbc3836b6ca8d8bfd772c302023643ecb52040
techx1 <ss.uitech01@gmail.com>
2022-10-20 17:05:15 +0500

    added a page

---
+++ src/pages/agriculture-products/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AgricultureProducts = () => {
+  return (
+    <div>AgricultureProducts</div>
+  )
+}
+
+export default AgricultureProducts
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>1sttradex</title>
+      </Head>
+      <div>Homepage</div>
+    </>
+  );
+}
