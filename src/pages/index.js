682a24c8121e356d2959f3c3bc3ffc12cd5da0c9
techx1 <ss.uitech01@gmail.com>
2022-10-20 16:54:13 +0500

    initial setup

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <p className='font-bold text-3xl'>Tailwind CSS ready</p>
+    </div>
+  )
+}
