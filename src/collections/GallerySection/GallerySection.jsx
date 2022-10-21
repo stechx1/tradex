dfcdd71d7a8bfaca11c6899624056b6a4147f99b
techx1 <ss.uitech01@gmail.com>
2022-10-21 20:10:07 +0500

    added gallery section

--- src/collections/GallerySection/GallerySection.jsx
+++ src/collections/GallerySection/GallerySection.jsx
@@ -1,3 +1,10 @@
-export const GallerySection = () => {
-  return <div>GallerySection</div>;
+/* eslint-disable @next/next/no-img-element */
+export const GallerySection = ({ images }) => {
+  return (
+    <div className='my-[70px] grid grid-cols-2 gap-x-4 gap-y-4'>
+      {images.map((img, index) => (
+        <img className="rounded" key={index} src={img} alt='img' />
+      ))}
+    </div>
+  );
 };
--- src/pages/ginger-supply/index.jsx
+++ src/pages/ginger-supply/index.jsx
@@ -8,6 +8,33 @@ import {
 import { BreadCrumbs } from '../../components';
 
 const GingerSupply = () => {
+  const images = [
+    '/images/ginger/ginger-1.jpeg',
+    '/images/ginger/ginger-2.jpeg',
+    '/images/ginger/ginger-3.jpeg',
+    '/images/ginger/ginger-4.jpeg',
+    '/images/ginger/ginger-5.png',
+    '/images/ginger/ginger-6.jpeg',
+    '/images/ginger/ginger-7.jpeg',
+    '/images/ginger/ginger-8.jpeg',
+    '/images/ginger/ginger-9.jpeg',
+    '/images/ginger/ginger-10.jpeg',
+    '/images/ginger/ginger-11.jpeg',
+    '/images/ginger/ginger-12.jpeg',
+    '/images/ginger/ginger-13.jpeg',
+    '/images/ginger/ginger-14.jpeg',
+    '/images/ginger/ginger-15.jpeg',
+    '/images/ginger/ginger-16.jpeg',
+    '/images/ginger/ginger-17.jpeg',
+    '/images/ginger/ginger-19.jpeg',
+    '/images/ginger/ginger-20.jpeg',
+    '/images/ginger/ginger-21.jpeg',
+    '/images/ginger/ginger-22.jpeg',
+    '/images/ginger/ginger-23.jpeg',
+    '/images/ginger/ginger-24.jpeg',
+    '/images/ginger/ginger-25.jpeg',
+    '/images/ginger/ginger-26.jpeg',
+  ];
   return (
     <div>
       <HeroSection
@@ -21,20 +48,22 @@ const GingerSupply = () => {
         <BreadCrumbs breadcrumbs={['ginger supply']} />
       </section>
 
-      <main className='px-14 bg-white grid grid-cols-2 md:grid-cols-3 gap-x-2'>
+      <main className='px-14 bg-white grid grid-cols-2 md:grid-cols-5 gap-x-8'>
         <section className='font-chivo'>
           <DepartmentSection />
         </section>
 
-        <section>
+        <section className='col-span-2'>
           <MainSection
             heading={'Our Service to you'}
             italicPara='“We supply and export Ginger to international buyers in many countries around the world.”'
           />
         </section>
 
-        <section className='font-chivo'>
-          <GallerySection />
+        {/* <div></div> */}
+
+        <section className='font-chivo col-span-2'>
+          <GallerySection images={images} />
         </section>
       </main>
     </div>
